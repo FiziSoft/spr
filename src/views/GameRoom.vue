d45b861d46a15090299c2edb1005e3261622c40c
OlehDziuba <oleg_dzuba@outlook.com>
2024-04-22 20:19:53 +0300

    x

--- src/views/GameRoom.vue
+++ src/views/GameRoom.vue
@@ -19,12 +19,23 @@ const room = reactive({
   players: [],
 })
 
-const websocket = new WebSocket(`ws://127.0.0.1:7000/start/${route.params.id}?name=${pName}`);
+const userHash = localStorage.getItem("hash")
+let websocket;
+
+if (userHash){
+  websocket = new WebSocket(`ws://127.0.0.1:7000/start/${route.params.id}?name=${pName}&player_hash=${userHash}`);
+} else {
+  websocket = new WebSocket(`ws://127.0.0.1:7000/start/${route.params.id}?name=${pName}`);
+}
+
 websocket.onmessage = function (event){
   const message = JSON.parse(event.data);
 
-  room.value = message["room"]
+  if (message.hash){
+    localStorage.setItem("hash", message.hash)
+  }
 
+  Object.assign(room, message["room"])
 }
 
 

0cdac9ffc2327b138e78b87a62d926a5d75fc2ea
OlehDziuba <oleg_dzuba@outlook.com>
2024-04-22 20:53:00 +0300

    dz

--- src/views/GameRoom.vue
+++ src/views/GameRoom.vue
@@ -1,4 +1,8 @@
 <template>
+  <div v-for="player in room.players" :key="player.id">
+
+  </div>
+
   <div>
     {{ room.name }}
     {{ room.players.length }}
@@ -28,12 +32,24 @@ if (userHash){
   websocket = new WebSocket(`ws://127.0.0.1:7000/start/${route.params.id}?name=${pName}`);
 }
 
+const gameState = ref("WaitPlayers");
+
 websocket.onmessage = function (event){
   const message = JSON.parse(event.data);
+
+  const eventType = message.event;
 
   if (message.hash){
     localStorage.setItem("hash", message.hash)
   }
+
+  // TODO:
+  if (eventType === "GameStarted"){
+    gameState.value = "GameStarted"
+  }
+  // TODO:
+  // else if (eventType == "BBB"){doBBB}
+
 
   Object.assign(room, message["room"])
 }

fb8f73d0783ff57fbdf1fd9e196aaf4346ae4080
OlehDziuba <oleg_dzuba@outlook.com>
2024-04-22 19:06:52 +0300

    Room reactive

--- src/views/GameRoom.vue
+++ src/views/GameRoom.vue
@@ -1,39 +1,30 @@
 <template>
-    {{ $route.params.id }}
-<div>
-    Game Room
-</div>
-<div>
-    {{ pName }}
-</div>
-<div>
-    <h2 v-if="currentState === 'WaitPlayers'">Waiting players</h2>
-    <h2 v-else-if="currentState === 'Game can be started'">Game can be started</h2>
-    <h2 v-else-if="currentState === 'WaitChoice'">WaitChoice</h2>
-    <h2>{{ currentState }}</h2>
-
-    <button v-if="currentState"></button>
-</div>
+  <div>
+    {{ room.name }}
+    {{ room.players.length }}
+  </div>
 </template>
 
 
 <script setup>
-import {onBeforeMount, ref} from 'vue'
+import {onBeforeMount, ref, reactive} from 'vue'
 import { useRoute } from 'vue-router';
 
 const pName = localStorage.getItem('playerName')
 const route = useRoute()
 
 let currentState = ref('WaitPlayers');
+const room = reactive({
+  name: "",
+  players: [],
+})
 
 const websocket = new WebSocket(`ws://127.0.0.1:7000/start/${route.params.id}?name=${pName}`);
 websocket.onmessage = function (event){
-  const message = event.data;
-  if (currentState.value === "WaitPlayers" && message === "Game can be started"){
+  const message = JSON.parse(event.data);
 
-    currentState.value = "WaitChoice"
-    alert(currentState.value)
-  }
+  room.value = message["room"]
+
 }
 
 
